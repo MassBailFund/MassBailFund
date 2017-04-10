74d6e84388b76e8ec4ca7e92075453686fe954d9
katee <hello@kate.io>
2017-04-09 23:15:56 -0400

    Add contacts table

---
+++ db/migrate/20170410031149_create_contacts.rb
@@ -0,0 +1,10 @@
+class CreateContacts < ActiveRecord::Migration[5.0]
+  def change
+    create_table :contacts do |t|
+      t.string :name
+      t.string :phone
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170410025413) do
+ActiveRecord::Schema.define(version: 20170410031149) do
 
   create_table "addresses", force: :cascade do |t|
     t.string   "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.string   "name"
+    t.string   "phone"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
