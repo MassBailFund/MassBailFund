83b5812466ffcab62c02d713ec55c9145d8e3a56
katee <hello@kate.io>
2017-04-11 11:42:29 -0400

    Allow anyone to create a CaseDetail

--- spec/models/ability_spec.rb
+++ spec/models/ability_spec.rb
@@ -5,5 +5,7 @@ describe Ability do
     let(:user) { nil }
 
     it { is_expected.not_to be_able_to(:manage, :all) }
+    it { is_expected.to be_able_to(:new, CaseDetail) }
+    it { is_expected.to be_able_to(:create, CaseDetail) }
   end
 end
