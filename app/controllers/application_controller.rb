ce6e0e18737cd8ef5f4483aeaf70a66beb6fdd9e
katee <hello@kate.io>
2017-04-12 19:03:03 -0400

    Track current_user when a model is updated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  before_action :set_paper_trail_whodunnit
+
   check_authorization unless: :devise_controller?
 end
