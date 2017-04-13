ea444a8f9c463829b4b92c300f8885ccd747363d
katee <hello@kate.io>
2017-04-12 22:26:54 -0400

    Appease rubocop

--- app/controllers/client_referrals_controller.rb
+++ app/controllers/client_referrals_controller.rb
@@ -11,9 +11,7 @@ class ClientReferralsController < ApplicationController
   end
 
   def create
-    if @client_referral.save
-      redirect_to after_create_path
-    end
+    redirect_to after_create_path if @client_referral.save
   end
 
   private
@@ -22,46 +20,38 @@ class ClientReferralsController < ApplicationController
     root_path
   end
 
+  def load_facilities
+    @facilities = Facility.all
+  end
+
   def create_params
-    params.require(:client_referral).permit(
-      :facility_id,
+    allowed_params = client_referral_param_names + attorney_param_names + client_param_names + third_party_param_names
+    params.require(:client_referral).permit(allowed_params)
+  end
 
-      :attorney_email,
-      :attorney_name,
-      :attorney_phone,
-      :attorney_address,
-
-      :client_birth_date,
-      :client_email,
-      :client_name,
-      :client_phone,
-      :client_address,
-
-      :third_party_relationship_to_client,
-      :third_party_name,
-      :third_party_phone,
-      :third_party_address,
-
-      :docket_number,
-      :charges,
-      :arraignment_date,
-      :appearance_date,
-      :bail_amount,
-      :gps_required,
-      :court,
-      :open_cases_description,
-      :warrants_description,
-      :support_person_description,
-      :released_upon_posting_description,
-      :school_description,
-      :employment_description,
-      :housing_description,
-      :history_description,
-      :additional_info_description
+  def client_referral_param_names
+    %i(
+      facility_id docket_number charges arraignment_date appearance_date
+      bail_amount gps_required court open_cases_description
+      warrants_description support_person_description
+      released_upon_posting_description school_description
+      employment_description housing_description history_description
+      additional_info_description
     )
   end
 
-  def load_facilities
-    @facilities = Facility.all
+  def attorney_param_names
+    %i(attorney_email attorney_name attorney_phone attorney_address)
+  end
+
+  def client_param_names
+    %i(client_birth_date client_email client_name client_phone client_address)
+  end
+
+  def third_party_param_names
+    %i(
+      third_party_relationship_to_client third_party_email third_party_name
+      third_party_phone third_party_address
+    )
   end
 end
