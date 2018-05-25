871faa4b9ba3e9ef37e8498a55438148dd33a70a
AllaHoffman <sm1th303@gmail.com>
2018-05-25 11:51:40 -0400

    highlights the tab you're on

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,13 @@
 # Methods to use in views.
 #
 module ApplicationHelper
+  def active_tab_class(*paths, isScopedClientsTab)
+   active = false
+   paths.each { |path| active ||= current_page?(path) }
+   if(!isScopedClientsTab)
+     active && params['scope'].nil? ? 'active' : ''
+   else
+     params['scope'].nil? ? '' : 'active'
+   end
+ end
 end
