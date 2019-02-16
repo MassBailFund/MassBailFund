96f225ddab42f4eccb3edd1267687942de630ec9
AllaHoffman <sm1th303@gmail.com>
2019-02-16 17:11:30 -0500

    flashes for success or failure creating and deleting users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,9 +2,25 @@ class Admin::UsersController < ApplicationController
   load_and_authorize_resource
 
   def create
-    User.create!({:email => params[:user][:email],
+    user = User.new({:email => params[:user][:email],
       :password => params[:user][:password],
       :password_confirmation => params[:user][:password] })
+    if user.save
+      flash[:success] = 'User created! You will need to send them their password'
+    else
+      flash[:danger] = 'Error creating user: ' + user.errors.full_messages.to_sentence
+    end
+    redirect_to settings_path
+  end
+
+  def destroy()
+    user = User.find(params[:id])
+    if user.destroy()
+      flash[:success] = 'User deleted!'
+    else
+      flash[:danger] = 'Error creating user: ' + user.errors.full_messages.to_sentence
+    end
+    redirect_to settings_path
   end
 
   def user_params
