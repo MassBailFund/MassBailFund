de12449c2f4d97ef8473ecb8be1d63ab8af8b482
AllaHoffman <sm1th303@gmail.com>
2019-03-03 22:23:34 -0500

    allow cross site iframe

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,6 +33,9 @@ Rails.application.configure do
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
+  config.action_dispatch.default_headers = {
+    'X-Frame-Options' => 'ALLOWALL'
+}
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
